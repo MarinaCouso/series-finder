12ed5687ba5b6014b2abbbc4663ea5916f87a5b8
MarinaCouso <marina.couso.martinez@gmail.com>
2020-03-20 09:33:27 +0100

    Listener to articles in the list + beggining of addToFavorites

--- js/main.js
+++ js/main.js
@@ -10,9 +10,6 @@ const listSection = document.querySelector('.js-listSection');
 const form = document.querySelector('.js-form');
 // 3. Funciones
 
-// 1ª: Escuchar la búsqueda y obtener datos de la API
-// 2ª: Pintar listado de series
-// 3ª: Escuchar el click en la lista de series
 // 4ª: Añadir de la lista de favoritas
 // 5ª: Pintar series en favoritas
 // 6ª:  Guardar en localStorage (y añadir al principio leer localStorage)
@@ -39,25 +36,53 @@ function addList(ev) {
     .then(data => {
       for (let i = 0; i < data.length; i++) {
         listSeries.push({
-          image: data[i].show.image.medium,
-          name: data[i].show.name
+          image: getImage(),
+          name: data[i].show.name,
+          id: data[i].show.id
         });
+      }
+      function getImage() {
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].show.image === null) {
+            image: 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+          } else {
+            image: data[i].show.image.medium;
+          }
+        }
       }
       paintSeries();
     });
   console.log(listSeries);
 }
 
-// PENDIENTE ARREGLAR PINTAR
+// 2ª: Pintar listado de series
 function paintSeries() {
+  listSection.innerHTML = '';
   for (let i = 0; i < listSeries.length; i++) {
-    listSection.innerHTML += '<article>';
-    listSection.innerHTML += '<img src="' + listSeries[i].image + '" alt="poster" />';
-    listSection.innerHTML += '<h3>' + listSeries[i].name + '</h3>';
-    listSection.innerHTML += '</article>';
+    listSection.innerHTML += `<article class="js-article article" id="${[i]}">`;
+    listSection.innerHTML += `<img src="${listSeries[i].image}" alt="Imagen de la serie ${listSeries[i].name}" />`;
+    listSection.innerHTML += `<h3>${listSeries[i].name}</h3>`;
+    listSection.innerHTML += `</article>`;
   }
+  listenToList();
+}
+
+// 3ª: Escuchar el click en la lista de series
+let clickListId;
+function listenToList() {
+  let seriesArticles = document.querySelectorAll('.js-article');
+
+  for (let i = 0; i < seriesArticles.length; i++) {
+    seriesArticles[i].addEventListener('click', addToFavorites);
+  }
+}
+// 4ª: Añadir de la lista de favoritas
+function addToFavorites(ev) {
+  clickListId = ev.target.id;
+  console.log(clickListId);
 }
 // 2. Listeners
 inputSearch.addEventListener('keyup', getValue);
 btnSearch.addEventListener('click', addList);
 form.addEventListener('click', preventDefault);
+listSection.addEventListener('click', listenToList());
